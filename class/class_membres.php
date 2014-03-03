1a3d9344dfadafd65a178cf69c9288c3e42db9dd
alvindsa <alvin.artus@groupe-dsa.com>
2014-03-03 09:30:58 +0100

    OKi
    Commit

--- class/class_membres.php
+++ class/class_membres.php
@@ -25,25 +25,31 @@ public function getMDP()
     return  $this->_mdp;
 }
 
+public function getID()
+{
+    return $this->_id;
+}
+
+public function setID($id)
+{
+    $this->_ID = $id;
+    return $this;
+}
 
 
 
+public function getNOM()
+{
+    return $this->_NOM;
+}
+
+public function setNOM($NOM)
+{
+    $this->_nom = $NOM;
+    return $this;
+}
 
 
-
-
-
-
-
-
-
-  public function setForce($force)  {
-    // On vérifie qu'on nous donne bien soit une « FORCE_PETITE », soit une « FORCE_MOYENNE », soit une « FORCE_GRANDE ».
-  if (in_array($force, array(self::FORCE_PETITE, self::FORCE_MOYENNE, self::FORCE_GRANDE)))
-   {
-      $this->_force = $force;
-   }
-  }
 
 
 
---
+++ test_class.php
@@ -0,0 +1,11 @@
+<?php
+
+include('class/class_membres.php');
+
+
+$membres = New Membres();
+
+$membres->setMDP('23');
+$membres->getMDP();
+
+?>
